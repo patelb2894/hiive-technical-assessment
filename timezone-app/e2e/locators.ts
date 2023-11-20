22c742c53f82f170ca6381ad492ac19787f8b3fa
patelb2894 <151471182+patelb2894@users.noreply.github.com>
2023-11-20 14:28:34 -0800

    [HIIVE]: Adding e2e tests for timezone app

---
+++ timezone-app/e2e/locators.ts
@@ -0,0 +1,8 @@
+export const SELECT_TIMEZONE = '#timezone';
+export const TABLE_BODY = 'tbody';
+export const TABLE_ROW = 'tr';
+export const TABLE_CELLS = 'td';
+export const PST_TIMEZONE = 'America/Los_Angeles';
+export const DELETE_BUTTON = 'text=Delete';
+export const LABEL_NAME = 'Bansari';
+export const TIMEZONE_VALUE = 'text=America/Los_Angeles';
--- timezone-app/playwright.config.ts
+++ timezone-app/playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: { headless: false },
     },
   ],
 });
